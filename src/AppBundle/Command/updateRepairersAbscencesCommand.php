5cdb0b44c664eec138f1a092a916d63c3582e8a9
Lloyd Dingalt <ldingalt@gmail.com>
2018-02-04 22:21:04 +0100

    Correction du namespace dans updateRepairersAbscencesCommand

--- src/AppBundle/Command/updateRepairersAbscencesCommand.php
+++ src/AppBundle/Command/updateRepairersAbscencesCommand.php
@@ -1,6 +1,6 @@
 <?php
 
-    namespace Hopital\AppBundle\Command;
+    namespace AppBundle\Command;
 
     use Symfony\Component\Console\Command\Command;
     use Symfony\Component\Console\Input\InputArgument;
